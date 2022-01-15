a091dd5842028fd9231cd5885f705fe9b4a55ada
Max Buchholz <Max.Buchholz@gmx.de>
2022-01-15 11:39:13 +0100

    Prerequisites prepare dataframes and calculate logarithm of computing power

--- exercise5/A5.5.py
+++ exercise5/A5.5.py
@@ -1,13 +1,21 @@
 """
 application for solving Exercise 5.5
 """
+import math
+
+import pandas
 
 
 def main():
     """
     main application
     """
-    pass
+    computer = pandas.read_csv("./data/GrosseKisten.csv")
+    processor = pandas.read_csv("./data/Prozessoren.csv")
+    computer["FLOPS"] = computer["FLOPS"].apply(lambda x: math.log10(x))
+    processor["FLOPS"] = processor["FLOPS"].apply(lambda x: math.log10(x))
+    print(computer)
+    print(processor)
 
 
 if __name__ == "__main__":
